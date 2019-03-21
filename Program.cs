b3b8a1ce10929f16bf96362221716b5fd15a6b1f
stoutman99 <45634563+stoutman99@users.noreply.github.com>
2019-03-21 09:57:33 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from John Stout!");
+        }
+    }
+}
